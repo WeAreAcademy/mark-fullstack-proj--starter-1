e94dc49e18a93013f4e7ce04679a08a876eb2fcd
nbogie <neillbogie@googlemail.com>
2022-09-05 22:33:52 +0100

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    },
+  extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    },
+  plugins: [
+        "@typescript-eslint"
+    ],
+  rules: {
+        "@typescript-eslint/ban-types": [
+            "error",
+            {
+        types: {
+                    "{}": false,
+                    // to avoid having to get into Record<string, never>
+                },
+            },
+        ],
+        "@typescript-eslint/no-empty-interface": 0
+    },
+};
