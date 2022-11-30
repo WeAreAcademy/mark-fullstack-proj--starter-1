5af6246c9d89ba1f7951e58a7130cb1c44004fe3
nbogie <neillbogie@googlemail.com>
2022-11-30 22:13:27 +0000

    add table-creation sql

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,1 +1,8 @@
--- your SQL goes here!
+DROP TABLE IF EXISTS food;
+
+CREATE TABLE  food (
+    id          serial PRIMARY KEY,
+    title       varchar(40) NOT NULL
+);
+
+INSERT INTO food (title) VALUES ('sourdough'), ('olive oil'), ('pizza'), ('picanha'), ('gorgonzola'), ('basil');
