8ddd9da9bac49ab8a8b38ee6ffc55c083170017b
nbogie <neillbogie@googlemail.com>
2023-08-24 22:14:46 +0100

    extend express Request in src/types/express

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,17 @@
+//This is an example of how to extend Express's Request interface.
+//E.g. perhaps you want to give some / all requests a requestId property
+//(The actual values might be set using some middleware, elsewhere.)
+
+//makes this a module
+export {};
+
+declare global {
+    namespace Express {
+        // These open interfaces may be extended in an application-specific manner via declaration merging,
+        //as stated here: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/12ceeef6ff63cbe744a4e6ade6ed7cf6cde15234/types/express-serve-static-core/index.d.ts#L18-L19
+        //
+        interface Request {
+            requestId?: string;
+        }
+    }
+}
